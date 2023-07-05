d8ee4451d02a8dd62079cad5cad835a43fa05dbb
Truc Nguyen <truc.nguyenht@gmail.com>
2023-07-06 01:02:46 +0700

    update code

--- src/ReactForm/QuanLySV.jsx
+++ src/ReactForm/QuanLySV.jsx
@@ -10,10 +10,10 @@ export default class QuanLySV extends Component {
       email: "",
     },
     error: {
-      maSV: "Mã sinh viên không được trống",
-      hoTen: "Họ tên không được trống",
-      soDT: "Số điện thoại không được trống",
-      email: "Email không được trống",
+      maSV: "",
+      hoTen: "",
+      soDT: "",
+      email: "",
     },
     dsSV: [
       {
@@ -25,16 +25,65 @@ export default class QuanLySV extends Component {
     ],
   };
 
+  formValid = () => {
+    // Chưa đúng format
+    let inValid = false;
+    Object.values(this.state.value).forEach((value) => {
+      if (!value) {
+        inValid = true;
+      }
+    });
+    return inValid;
+    console.log(Object.keys(this.state.value));
+  };
+
   handleOnChange = (event) => {
+    const { name, value, pattern } = event.target;
+    const newSV = { ...this.state.value, [name]: value };
+    const newError = { ...this.state.error };
+    // xử lí lỗi
+    if (!value.trim()) {
+      // Không có value
+      newError[name] = "Trường này không được để trống !";
+    } else {
+      // validate value nhập vào có đúng format?
+      if (pattern) {
+        const regex = new RegExp(pattern);
+        const inValid = regex.test(value);
+        if (!inValid) {
+          newError[name] = "Mã sinh viên không đúng định dạng";
+        } else {
+          // có input value vào
+          newError[name] = "";
+        }
+      }
+    }
+    this.setState({ value: newSV, error: newError });
+  };
+
+  handleOnBlur = (event) => {
     const { name, value } = event.target;
     const newSV = { ...this.state.value, [name]: value };
-    this.setState({ value: newSV });
+    const newError = { ...this.state.error };
+    // xử lí lỗi
+    if (!value.trim()) {
+      // Không có value
+      newError[name] = "Trường này không được để trống !";
+    } else {
+      newError[name] = "";
+    }
+    this.setState({ value: newSV, error: newError });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const newSV = [...this.state.dsSV, this.state.value];
-    this.setState({ dsSV: newSV });
+
+    // xử lí validate form
+    if (!this.formValid()) {
+      const newSV = [...this.state.dsSV, this.state.value];
+      this.setState({ dsSV: newSV });
+    }
+
     console.log(this.state.value);
   };
 
@@ -55,14 +104,18 @@ export default class QuanLySV extends Component {
                     Mã SV <span className="text-danger">*</span>
                   </label>
                   <input
-                    required
                     type="text"
                     value={maSV}
                     className="form-control"
                     name="maSV"
                     id="maSV"
                     placeholder="Mã SV"
+                    // thực hiện input xong rồi xóa nội dung input
                     onChange={this.handleOnChange}
+                    // thao tác focus out
+                    onBlur={this.handleOnBlur}
+                    // pattern được phép nhập
+                    pattern="^[0-9]{1,5}$"
                   />
                 </div>
                 {error.maSV && (
@@ -74,7 +127,6 @@ export default class QuanLySV extends Component {
                   Họ và Tên <span className="text-danger">*</span>
                 </label>
                 <input
-                  required
                   type="text"
                   value={hoTen}
                   className="form-control"
@@ -82,6 +134,7 @@ export default class QuanLySV extends Component {
                   id="hoTen"
                   placeholder="Họ và Tên"
                   onChange={this.handleOnChange}
+                  onBlur={this.handleOnBlur}
                 />
                 {error.hoTen && (
                   <span className="text text-danger">{error.hoTen}</span>
@@ -95,7 +148,6 @@ export default class QuanLySV extends Component {
                     Số điện thoại <span className="text-danger">*</span>
                   </label>
                   <input
-                    required
                     type="text"
                     value={soDT}
                     className="form-control"
@@ -103,6 +155,7 @@ export default class QuanLySV extends Component {
                     id="soDT"
                     placeholder="Số điện thoại"
                     onChange={this.handleOnChange}
+                    onBlur={this.handleOnBlur}
                   />
                 </div>
                 {error.soDT && (
@@ -114,7 +167,6 @@ export default class QuanLySV extends Component {
                   Email <span className="text-danger">*</span>
                 </label>
                 <input
-                  required
                   type="text"
                   value={email}
                   className="form-control"
@@ -122,6 +174,7 @@ export default class QuanLySV extends Component {
                   id="email"
                   placeholder="Email"
                   onChange={this.handleOnChange}
+                  onBlur={this.handleOnBlur}
                 />
                 {error.email && (
                   <span className="text text-danger">{error.email}</span>
